6e4b5fb154d80394063e38d139fc9f20b72de223
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2023-08-30 00:29:29 +0200

    Annotation work

--- hibernate-based-migration/src/main/java/dev/bodewig/hibernate_based_migration/HibernateMigration.java
+++ hibernate-based-migration/src/main/java/dev/bodewig/hibernate_based_migration/HibernateMigration.java
@@ -1,7 +1,25 @@
+package dev.bodewig.hibernate_based_migration;
+
+import java.io.File;
+import org.flywaydb.core.api.CoreMigrationType;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
+import org.flywaydb.core.extensibility.MigrationType;
 
 public abstract class HibernateMigration extends BaseJavaMigration {
+
+	protected File configOld;
+
+	protected File configNew;
+
+	@Override
+	protected void init() {
+		super.init();
+		Migration migration = this.getClass().getAnnotation(Migration.class);
+		this.configOld = new File(migration.fromCfg());
+		this.configNew = new File(migration.toCfg());
+	}
+
 	@Override
 	public void migrate(Context context) throws Exception {
 		// TODO Auto-generated method stub
@@ -15,5 +33,10 @@ public abstract class HibernateMigration extends BaseJavaMigration {
 		 * update.execute("UPDATE person SET name='" + anonymizedName + "' WHERE id=" +
 		 * id); } } } }
 		 */
+	}
+
+	@Override
+	public MigrationType getType() {
+		return CoreMigrationType.CUSTOM;
 	}
 }
