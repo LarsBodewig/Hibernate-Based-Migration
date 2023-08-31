8e3b2a8cc6464c13a39d06a48a4f96ba1f36d734
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2023-08-31 21:30:02 +0200

    Abstract HibernateMigration

--- hibernate-based-migration/src/main/java/dev/bodewig/hibernate_based_migration/HibernateMigration.java
+++ hibernate-based-migration/src/main/java/dev/bodewig/hibernate_based_migration/HibernateMigration.java
@@ -1,42 +1,52 @@
 package dev.bodewig.hibernate_based_migration;
 
-import java.io.File;
 import org.flywaydb.core.api.CoreMigrationType;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.flywaydb.core.extensibility.MigrationType;
+import org.hibernate.SessionFactory;
+import org.hibernate.StatelessSession;
+import org.hibernate.cfg.Configuration;
 
 public abstract class HibernateMigration extends BaseJavaMigration {
 
-	protected File configOld;
+	protected Configuration configBefore;
 
-	protected File configNew;
+	protected Configuration configAfter;
 
 	@Override
 	protected void init() {
 		super.init();
-		Migration migration = this.getClass().getAnnotation(Migration.class);
-		this.configOld = new File(migration.fromCfg());
-		this.configNew = new File(migration.toCfg());
-	}
-
-	@Override
-	public void migrate(Context context) throws Exception {
-		// TODO Auto-generated method stub
-
-		/*
-		 * try (Statement select = context.getConnection().createStatement()) { try
-		 * (ResultSet rows = select.executeQuery("SELECT id FROM person ORDER BY id")) {
-		 * while (rows.next()) { int id = rows.getInt(1); String anonymizedName =
-		 * "Anonymous" + id; try (Statement update =
-		 * context.getConnection().createStatement()) {
-		 * update.execute("UPDATE person SET name='" + anonymizedName + "' WHERE id=" +
-		 * id); } } } }
-		 */
+		configBefore = configBefore();
+		configAfter = configAfter();
 	}
 
 	@Override
 	public MigrationType getType() {
-		return CoreMigrationType.CUSTOM;
+		return CoreMigrationType.JDBC;
 	}
+
+	@Override
+	public void migrate(Context context) throws Exception {
+		try (SessionFactory factoryBefore = configBefore.buildSessionFactory();
+				SessionFactory factoryAfter = configAfter.buildSessionFactory()) {
+			try (StatelessSession sessionBefore = factoryBefore.openStatelessSession(context.getConnection())) {
+				doBefore(sessionBefore);
+			}
+			doSql(context);
+			try (StatelessSession sessionAfter = factoryAfter.openStatelessSession(context.getConnection())) {
+				doAfter(sessionAfter);
+			}
+		}
+	}
+
+	public abstract Configuration configBefore();
+
+	public abstract Configuration configAfter();
+
+	public abstract void doBefore(StatelessSession session) throws Exception;
+
+	public abstract void doSql(Context context) throws Exception;
+
+	public abstract void doAfter(StatelessSession session) throws Exception;
 }
